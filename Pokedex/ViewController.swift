8132821a683b11d3678c1c1fbdd8bd9f67de74b2
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-29 16:03:22 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Luiz Henrique Silva Sampaio on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
